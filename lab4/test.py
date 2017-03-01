76b3f10a81dd18e63f3a193d8a54b8ab8ec3e9d8
Pierre Karashchuk <krchtchk@gmail.com>
2017-02-28 16:23:54 -0800

    add lab 4 materials

---
+++ lab4/test.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+N = 5
+men_means = (20, 35, 30, 35, 27)
+men_std = (2, 3, 4, 1, 2)
+
+ind = np.arange(N)  # the x locations for the groups
+width = 0.35       # the width of the bars
+
+fig, ax = plt.subplots()
+rects1 = ax.bar(0, 10, width, color='r')
+
+# women_means = (25, 32, 34, 20, 25)
+# women_std = (3, 5, 2, 3, 3)
+# rects2 = ax.bar(ind + width, women_means, width,
+#                 color='y', yerr=women_std)
+
+# add some text for labels, title and axes ticks
+ax.set_ylabel('Scores')
+ax.set_title('Scores by group and gender')
+# ax.set_xticks(ind + width / 2)
+ax.set_xticklabels(('G1', 'G2', 'G3', 'G4', 'G5'))
+
+# ax.legend((rects1[0], rects2[0]), ('Men', 'Women'))
+
+
+def autolabel(rects):
+    """
+    Attach a text label above each bar displaying its height
+    """
+    for rect in rects:
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
+                '%d' % int(height),
+                ha='center', va='bottom')
+
+# autolabel(rects1)
+# autolabel(rects2)
+
+plt.show(block=False)
