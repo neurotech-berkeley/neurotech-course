2613f27ea3324162e9f2eadfa60df903b443d730
Deven Navani <deven.navani@gmail.com>
2020-04-12 16:50:13 -0700

    remove old labs

--- old_labs/lab3/paradigm/screen.py
+++
@@ -1,8 +0,0 @@
-import pygame
-from pygame.locals import *
-from constants import *
-
-if fullscreen_on:
-    screen = pygame.display.set_mode(size, FULLSCREEN)
-else:
-    screen = pygame.display.set_mode(size)
